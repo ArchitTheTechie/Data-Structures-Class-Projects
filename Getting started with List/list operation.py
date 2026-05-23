89a10299f228a6ea81b4c06426511cfc7e8a781a
Archit Anant <maneesha.roy09@gmail.com>
2026-05-23 16:47:24 +0530

    Add files via upload

---
+++ Getting started with List/list operation.py
@@ -0,0 +1,29 @@
+fruits = ["Mango", "Banana", "Apple", "Grapes", "Orange"]
+
+#length of the list
+print(len(fruits))
+
+#for i in fruits:
+#   print(i)
+
+#access one item from the list
+print(fruits[1])
+
+#slicing the list
+print(fruits[1:4])
+
+#add item/s in this list
+fruits.append("Kiwi")
+print(fruits)
+
+#remove item from the list
+fruits.remove("Grapes")
+print(fruits)
+
+#reverse the list
+fruits.reverse()
+print(fruits)
+
+#sort the list
+fruits.sort()
+print(fruits)
